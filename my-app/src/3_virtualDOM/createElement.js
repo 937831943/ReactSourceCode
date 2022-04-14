f470e99d3aa1947c637b46ee82d48a5e67a99433
LengZixuan <jyx13164683568@163.com>
2022-04-14 15:47:01 +0800

    调整文件目录结构

---
+++ my-app/src/3_virtualDOM/createElement.js
@@ -0,0 +1,24 @@
+/**
+ * 创建元素（虚拟DOM）的方法
+ * @param {*} type 元素的类型（标签）（DOM）
+ * @param {*} config 配置对象属性key ref
+ * @param  {...any} children 放着所有的儿子，这里会做成一个数组
+ */
+function createElement (type, config, ...children) {
+    return {
+        type,
+        props: {
+            ...config,
+            children: children.map(ele => {
+                let child = ele
+                if (typeof child !== 'object') {
+                    child = {
+                        type: Symbol.for('ELEMENT_TEXT'),
+                        props: { text: ele, children: [] }
+                    }
+                }
+                return child
+            })
+        }
+    }
+}
